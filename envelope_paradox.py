b713da23f92a4d3f6612504f743c040d3e9698a6
Torbjørn B Ness <torbness@gmail.com>
2012-08-01 18:42:38 +0200

    Final version
    Here I found that if i scramble the array,
    it does not work!

--- envelope_paradox.py
+++ envelope_paradox.py
@@ -1,5 +1,5 @@
 #This is a simulation to solve the two envolope paradox
-from random import choice, randint
+from random import choice, randint, shuffle
 
 def return_double_or_half(orig_value):
     return choice([.5, 2.]) * orig_value
@@ -15,18 +15,17 @@ def expectation_value_test():
     print change_env_value/keep_env_value
 
 def choise_test():
-    n_steps = 100000
+    n_steps = 10000
     keep_env_value = 0
     change_env_value = 0
     for n in xrange(n_steps):
         start_value = randint(1,1000)
         values = [start_value, return_double_or_half(start_value)]
+        shuffle(values) # This is the critical line!
         keep_env_value += values[0]
         change_env_value += values[1]
     print change_env_value/keep_env_value
 
-
-   
 if __name__ == '__main__':
     #expectation_value_test()
     choise_test()
