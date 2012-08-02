92f81d8fb569d8c6c28bb165aec710c4d7594c35
Torbjørn B Ness <torbness@gmail.com>
2012-08-02 22:07:57 +0200

    Clean-up. Final final version

--- envelope_paradox.py
+++ envelope_paradox.py
@@ -4,16 +4,6 @@ from random import choice, randint, shuffle
 def return_double_or_half(orig_value):
     return choice([.5, 2.]) * orig_value
 
-def expectation_value_test():
-    a = 10
-    n_steps = 10000
-    keep_env_value = 0
-    change_env_value = 0
-    for n in xrange(n_steps):
-        keep_env_value += a
-        change_env_value += return_double_or_half(a)
-    print change_env_value/keep_env_value
-
 def choise_test():
     n_steps = 10000
     keep_env_value = 0
@@ -29,5 +19,4 @@ def choise_test():
     print change_env_value/keep_env_value
 
 if __name__ == '__main__':
-    #expectation_value_test()
     choise_test()
