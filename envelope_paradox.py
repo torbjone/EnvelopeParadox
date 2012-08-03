ef8ed0d3e76ddaa9d5f023c0580b5fb535b837a6
Mike Hull <mikehulluk@googlemail.com>
2012-08-03 03:02:48 -0500

    Plot out the distributions of values[0] and values[1]

--- envelope_paradox.py
+++ envelope_paradox.py
@@ -1,5 +1,7 @@
 #This is a simulation to solve the two envolope paradox
 from random import choice, randint, shuffle
+import pylab
+import numpy as np
 
 def return_double_or_half(orig_value):
     return choice([.5, 2.]) * orig_value
@@ -9,6 +11,8 @@ def choise_test():
     keep_env_value = 0
     change_env_value = 0
     num_first_val_greater_than_second = 0
+    v0_vals = []
+    v1_vals = []
     for n in xrange(n_steps):
         start_value = randint(1,1000)
         values = [start_value, return_double_or_half(start_value)]
@@ -19,8 +23,19 @@ def choise_test():
         #shuffle(values) 
         keep_env_value += values[0]
         change_env_value += values[1]
+
+        v0_vals.append(values[0])
+        v1_vals.append(values[1])
     print change_env_value/keep_env_value
     print 'Prop value[0] > value[1]', float(num_first_val_greater_than_second) / n_steps
+
+    f = pylab.figure()
+    ax = f.add_subplot(111)
+    bins = np.linspace(0,2000,num=20)
+    ax.hist(v0_vals, bins=bins, alpha=0.4, label='Dist of V[0]')
+    ax.hist(v1_vals, bins=bins, alpha=0.4, label='Dist of V[1]')
+    ax.legend()
+    pylab.show()
 
 if __name__ == '__main__':
     choise_test()
